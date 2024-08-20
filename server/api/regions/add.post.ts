cd6637f73ca3a2b0e8c2ecbcd0037f6d92b78b33
Ömrüm Baki Temiz <omrumbakitemiz@icloud.com>
2024-08-20 20:57:12 +0300

    do not allow region add on prod

--- server/api/regions/add.post.ts
+++ server/api/regions/add.post.ts
@@ -1,6 +1,10 @@
 import { db, Regions } from "~~/server/drizzle/schema";
 
-export default defineEventHandler(async (event) => {
+export default defineEventHandler(async () => {
+  // if env is not development, don't allow adding regions
+  if (process.env.NODE_ENV !== "development") {
+    throw new Response("Not allowed", { status: 403 });
+  }
 
   // check if the regions already exists
   const allRegions = await db.select().from(Regions);
