13b7a2ff9c36a1f22525a3cbdde50ecd4968718e
Ömrüm Baki Temiz <omrumbakitemiz@icloud.com>
2024-08-31 17:37:14 +0300

    remove 10 item limit on all items endpoint

--- server/api/items/all.ts
+++ server/api/items/all.ts
@@ -2,13 +2,12 @@ import { db, Items } from "~~/server/drizzle/schema";
 
 export default defineCachedEventHandler(
   async () => {
-    // return await db.select().from(Items);
-    return await db.select().from(Items).limit(10);
+    return await db.select().from(Items);
   },
   {
     name: "getAllItems",
     maxAge: 60 * 60 * 24 * 365, // 1 year
     getKey: () => "items",
-    // shouldBypassCache: () => process.env.NODE_ENV === "development",
+    shouldBypassCache: () => process.env.NODE_ENV === "development",
   }
 );
